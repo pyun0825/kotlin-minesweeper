261e38b078f74ab1debec0ad1459872a1a69146b
pyun0825 <pyun0825@naver.com>
2023-02-14 22:57:45 +0900

    feat: add surrounding mine count logic

--- src/main/kotlin/domain/MineMap.kt
+++ src/main/kotlin/domain/MineMap.kt
@@ -23,15 +23,43 @@ class MineMap (private val width: Width,
         mapGeneratingStrategy
     )
 
-    private fun isCoordinateInsideMap(coordinate: Coordinate): Boolean {
-        return height.isRowBetween(coordinate) && width.isColBetween(coordinate)
+    private fun validateThatCoordinateIsInsideMap(coordinate: Coordinate) {
+        if (height.isRowBetween(coordinate) && width.isColBetween(coordinate)) {
+            throw IllegalArgumentException("Coordinate is outside of map.")
+        }
     }
 
     fun getTileStatusInCoordinate(coordinate: Coordinate): TileStatus {
-        if (!isCoordinateInsideMap(coordinate)) {
-            throw IllegalArgumentException("Coordinate is outside of map.")
-        }
+        validateThatCoordinateIsInsideMap(coordinate)
         return mineMap[coordinate.row][coordinate.col].status
+    }
+
+    fun getNumberOfMinesAroundCoordinate(coordinate: Coordinate): Int {
+        validateThatCoordinateIsInsideMap(coordinate)
+        val startCoordinateOfSubMap = getStartCoordinateOfSubMap(coordinate)
+        val endCoordinateOfSubMap = getEndCoordinateOfSubMap(coordinate)
+        var count = 0
+
+        for (row in startCoordinateOfSubMap.row..endCoordinateOfSubMap.row) {
+            for (col in startCoordinateOfSubMap.col..endCoordinateOfSubMap.col) {
+                if (mineMap[row][col].status == TileStatus.MINE) {
+                    count++
+                }
+            }
+        }
+        return count
+    }
+
+    private fun getStartCoordinateOfSubMap(coordinate: Coordinate): Coordinate {
+        val startRow = maxOf(0, coordinate.row - 1)
+        val startCol = maxOf(0, coordinate.col - 1)
+        return Coordinate(startRow, startCol)
+    }
+
+    private fun getEndCoordinateOfSubMap(coordinate: Coordinate): Coordinate {
+        val endRow = minOf(height.height - 1, coordinate.row + 1)
+        val endCol = minOf(width.width - 1, coordinate.col + 1)
+        return Coordinate(endRow, endCol)
     }
 
     fun getMaxRowNum(): Int {
--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -2,6 +2,7 @@ package view
 
 import domain.Coordinate
 import domain.MineMap
+import domain.TileStatus
 
 object OutputView {
     private const val MINE_MAP_MESSAGE = "==========현재 지뢰밭 상태=========="
@@ -16,7 +17,15 @@ object OutputView {
 
     private fun printMineMapRow(mineMap: MineMap, rowNum: Int) {
         for (colNum in 0 .. mineMap.getMaxColNum()) {
-            print(mineMap.getTileStatusInCoordinate(Coordinate(rowNum, colNum)).displayChar + " ")
+            printMineMapTile(mineMap, Coordinate(rowNum, colNum))
         }
+    }
+
+    private fun printMineMapTile(mineMap: MineMap, coordinate: Coordinate) {
+        if (mineMap.getTileStatusInCoordinate(coordinate) == TileStatus.NORMAL) {
+            print(mineMap.getNumberOfMinesAroundCoordinate(coordinate).toString() + " ")
+            return
+        }
+        print(mineMap.getTileStatusInCoordinate(coordinate).displayChar + " ")
     }
 }
