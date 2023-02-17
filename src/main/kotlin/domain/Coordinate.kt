06af2d2b72a482d938d88e85d17de5a5adee010b
pyun0825 <pyun0825@naver.com>
2023-02-17 13:57:45 +0900

    feat: add coordinate input logic

--- src/main/kotlin/domain/Coordinate.kt
+++ src/main/kotlin/domain/Coordinate.kt
@@ -1,3 +1,8 @@
 package domain
 
-data class Coordinate(val row: Int, val col: Int)
+data class Coordinate(val row: Int, val col: Int) {
+    constructor(rowColString: String) : this(
+        rowColString.split(",")[0].toInt(),
+        rowColString.split(",")[1].toInt()
+    )
+}
--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,11 +1,13 @@
 package view
 
+import domain.Coordinate
 import dto.MineMapRequestDto
 
 object InputView {
     private const val GET_HEIGHT_MESSAGE = "높이를 입력하세요."
     private const val GET_WIDTH_MESSAGE = "너비를 입력하세요."
     private const val GET_MINE_COUNT = "지뢰는 몇 개인가요?"
+    private const val GET_TARGET_COORD = "오픈할 좌표를 입력해 주세요 row, col ex) 5, 4"
 
     fun getInitialInput(): MineMapRequestDto {
         println(GET_WIDTH_MESSAGE)
@@ -15,5 +17,11 @@ object InputView {
         println(GET_MINE_COUNT)
         val mineCount = readln().toInt()
         return MineMapRequestDto(width, height, mineCount)
+    }
+
+    fun getTargetCoordinate(): Coordinate {
+        println(GET_TARGET_COORD)
+        val rowColString = readln()
+        return Coordinate(rowColString)
     }
 }
